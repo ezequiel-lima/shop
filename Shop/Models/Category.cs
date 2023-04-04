05d3f6afacfb7696d0f7b89440cacc3ee5e87a8f
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2023-04-04 20:36:10 -0300

    Add project files.

---
+++ Shop/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿namespace Shop.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+
+        }
+
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
